e03b76e53009cc4f6dc2ed81c649d18cb2a10ed4
Paul Smith <paulcsmith0218@gmail.com>
2017-09-08 12:00:53 -0400

    Initial commit

---
+++ config/development.cr
@@ -0,0 +1,2 @@
+LuckyRecord::Repo.db_name = "constable_crystal_development"
+LuckyMigrator::Runner.db_name = LuckyRecord::Repo.db_name
---
+++ spec/constable-crystal_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Constable::Crystal do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
