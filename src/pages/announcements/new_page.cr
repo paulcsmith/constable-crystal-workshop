6faeb8479e3b90e661d48600e35f939cf623a71b
Paul Smith <paulcsmith0218@gmail.com>
2017-09-08 15:06:11 -0400

    Show nicer error when creating announcements

--- src/pages/announcements/new_page.cr
+++ src/pages/announcements/new_page.cr
@@ -3,20 +3,16 @@ class Announcements::NewPage < BasePage
 
   render do
     h1 "Create a new announcement"
-    render_form_errors(@announcement_form) if @announcement_form.save_failed?
     render_announcement_form(@announcement_form)
-  end
-
-  private def render_form_errors(f)
-    p f.errors.join(", ")
   end
 
   private def render_announcement_form(f)
     form_for Announcements::Create do
       label_for f.title
       text_input f.title, autofocus: "true"
+      errors_for f.title
 
-      input type: "submit", value: "Post announcement"
+      submit "Post announcement"
     end
   end
 end
