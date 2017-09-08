48748555fba76cf3b7e79e5565fed47bff473ae5
Paul Smith <paulcsmith0218@gmail.com>
2017-09-08 16:57:34 -0400

    Validate length of the title

--- src/forms/announcement_form.cr
+++ src/forms/announcement_form.cr
@@ -1,3 +1,13 @@
 class AnnouncementForm < Announcement::BaseForm
   allow title
+
+  def call
+    validate_length title, min: 10
+  end
+
+  private def validate_length(field, min)
+    if (field.value || "").size < min
+      field.add_error "is too short"
+    end
+  end
 end
