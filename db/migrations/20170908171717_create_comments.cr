5ecefbe1de4d187c05a8c4c0129d0837cbdb0d33
Paul Smith <paulcsmith0218@gmail.com>
2017-09-09 09:39:01 -0400

    Format code. Update Lucky

--- db/migrations/20170908171717_create_comments.cr
+++ db/migrations/20170908171717_create_comments.cr
@@ -1,9 +1,9 @@
 class CreateComments::V20170908171717 < LuckyMigrator::Migration::V1
   def migrate
     create :comments do
-     add :author, String, optional: true
-     add :body, String
-     add :announcement_id, Int32
+      add :author, String, optional: true
+      add :body, String
+      add :announcement_id, Int32
     end
   end
 
